b63b6e29535c765db19a0de623788003444ce77b
Hugo Hörnquist <hugo@lysator.liu.se>
2025-04-19 18:23:27 +0200

    "standalone" eslint now ignores build files.

--- preambles/eslint.js
+++ preambles/eslint.js
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint'
 
 export default [
   {files: ['*.ts']},
+  {ignores: ['build/*.js']},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
