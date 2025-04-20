608f8c96259bca3ce4f209f3abfad0104a20bd89
Hugo Hörnquist <hugo@lysator.liu.se>
2025-04-20 17:20:19 +0200

    Remove unused SetMap.

--- src/multi-map.ts
+++ src/multi-map.ts
@@ -1,4 +1,4 @@
-export { MultiMap, SetMap }
+export { MultiMap }
 
 /**
 A mapping between keys, and lists of values.
@@ -26,32 +26,5 @@ class MultiMap<K, V> {
   /** Returns a new Map, matching the contentsn of this MultiMap. */
   toMap(): Map<K, V[]> {
     return new Map(this.#map)
-  }
-}
-
-class SetMap<K, V> {
-
-  #map: Map<K, Set<V>> = new Map
-
-  get(k: K) { return this.#map.get(k) }
-  delete(k: K) { return this.#map.delete(k) }
-  get size() { return this.#map.size }
-
-  push(k: K, ...vs: V[]) {
-    let set = this.#map.get(k)
-    if (!set) {
-      set = new Set
-      this.#map.set(k, set)
-    }
-
-    for (const v of vs) {
-      set.add(v)
-    }
-  }
-
-  *[Symbol.iterator]() {
-    for (const e of this.#map) {
-      yield e
-    }
   }
 }
